51313c47dea4f93e2fa12515446356a5e1378f0d
Lynn Gathoni <lynngathoni9@gmail.com>
2025-06-09 19:24:59 +0300

    Add all changes

--- script.js
+++ script.js
@@ -111,3 +111,14 @@ window.addEventListener('load',() => {
       visitCountElement.textContent = visitCount;
       }
     })
+    document.addEventListener('DOMContentLoaded', () => {
+      const mobileMenu = document.getElementById('mobile-menu');
+      const navLinks = document.getElementById('nav-links');
+
+      if(mobileMenu && navLinks) {
+        mobileMenu.addEventListener('click', () => {
+          navLinks.classList.toggle('active');
+          mobileMenu.classList.toggle('active');
+        });
+      }
+    })
